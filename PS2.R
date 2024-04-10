a58fe8d10cf8cbd6295c52a68cc353587d5c7d6b
ajrp92 <aj.rodriguezp1@uniandes.edu.co>
2024-04-09 23:52:55 -0500

    Update PS2.R
    Actualizado con modelo Logit. Sirve de prueba para otros modelos.

--- PS2.R
+++ PS2.R
@@ -15,36 +15,113 @@ train_personas <- read.csv("train_personas.csv")
 test_hogares <- read.csv("test_hogares.csv")
 test_personas <- read.csv("test_personas.csv")
 
-##Función de procesamiento
+##Función de procesamiento personas
 
 process_personas <- function(data, ...) {
-  
-  t_personas <- t_personas %>% mutate(
+  train_personas <- train_personas %>% mutate(
+  Niveleduc = ifelse(P6210==9,0,P6210),
+  Segsoc = ifelse(P6100==4,3,P6100),
+  Tiempotrab = P6426,
+  Ocupado = ifelse(is.na(Oc),0,1),
+  genero = P6020,
+  pension = P6920,
+  jefe = ifelse(P6050==1,1,0)
+  ) %>% 
+    select(id,Orden,Niveleduc,Segsoc,Tiempotrab,Ocupado,genero,pension,jefe)
     
-    
-  )
-  select()
 }
-
-process_hogares <- function(data, ...) {
   
-  t_hogares <- t_hogares %>% mutate(
-    
-    
+  train_personas <- process_personas(train_personas)
+  test_personas <- process_personas(test_personas)
+
+
+##Modelo1 #Train
+
+train_personas_nivel_hogar<- train_personas %>% 
+  group_by(id) %>% 
+  summarize(njefe=sum(jefe,na.rm=TRUE),
+            seguridad=sum(Segsoc,na.rm=TRUE),
+            Pension=sum(pension,na.rm=TRUE),
+            maxEducLevel=max(Niveleduc,na.rm=TRUE),
+            nocupados=sum(Ocupado,na.rm=TRUE)
   )
-  select()
-}
 
-##Formato de predicción
-  ##Predicción
+train_personas_hogar<- train_personas %>% 
+  filter(jefe==1) %>% 
+  select(id,Segsoc,Niveleduc,Ocupado,pension,genero) %>% 
+  rename(jefe_Educ_level=Niveleduc,
+         jefe_ocupado=Ocupado,
+         jefe_pension=pension,
+         jefe_genero=genero,
+         jefe_seguridad=Segsoc) %>% 
+  left_join(train_personas_nivel_hogar)
 
-predict <- basededatos %>% 
-  mutate(pobre_ = predict(modelo, newdata = basededatos, type = "raw"
-                          ) %>% select(id, pobre_)
+##Modelo1 #Test
+
+test_personas_nivel_hogar<- test_personas %>% 
+  group_by(id) %>% 
+  summarize(njefe=sum(jefe,na.rm=TRUE),
+            seguridad=sum(Segsoc,na.rm=TRUE),
+            Pension=sum(pension,na.rm=TRUE),
+            maxEducLevel=max(Niveleduc,na.rm=TRUE),
+            nocupados=sum(Ocupado,na.rm=TRUE)
+  )
+
+test_personas_hogar<- train_personas %>% 
+  filter(jefe==1) %>% 
+  select(id,Segsoc,Niveleduc,Ocupado,pension,genero) %>% 
+  rename(jefe_Educ_level=Niveleduc,
+         jefe_ocupado=Ocupado,
+         jefe_pension=pension,
+         jefe_genero=genero,
+         jefe_seguridad=Segsoc) %>% 
+  left_join(test_personas_nivel_hogar)
+
+##Ajustar bases hogares
+
+train_hogares <- train_hogares %>% select(id, Dominio, Pobre)
+test_hogares <- test_hogares %>% select(id,Dominio,)
+
+
+## Juntar bases
+
+train<- train_hogares %>% 
+  left_join(train_personas_hogar) %>% 
+  select(-id) #no longer need id
+
+test<- test_hogares %>% 
+  left_join(test_personas_hogar)
+
+## A factores
+
+train<- train %>% 
+  mutate(Pobre=factor(Pobre,levels=c(0,1),labels=c("No","Yes")),
+         Dominio=factor(Dominio),
+         jefe_Educ_level=factor(jefe_Educ_level,levels=c(0:6), labels=c("Ns",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria')),
+         maxEducLevel=factor(maxEducLevel,levels=c(0:6), labels=c("Ns",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria'))
+  )
+
+test<- test %>% 
+  mutate(Pobre=factor(Pobre,levels=c(0,1),labels=c("No","Yes")),
+         Dominio=factor(Dominio),
+         jefe_Educ_level=factor(jefe_Educ_level,levels=c(0:6), labels=c("Ns",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria')),
+         maxEducLevel=factor(maxEducLevel,levels=c(0:6), labels=c("Ns",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria'))
+  )
+
+##Modelo1
+
+modelo1 <- glm(Pobre ~Dominio + jefe_Educ_level + maxEducLevel + jefe_Ocupado + jefe_pension + jefe_seguridad)
+
+   ##Predicción
+
+predictSample <- test   %>% 
+  mutate(pobre_lab = predict(modelo1, newdata = test, type = "raw")    ## predicted class labels
+  )  %>% select(id,pobre_lab)
+
+
     ##Formato Kaggle
-    predict <- predict %>% 
-      mutate(pobre = ifelse(pobre_=="Yes",1,0))
-       %>% select(id, pobre)
+    predictSample <- predictSample %>% 
+      mutate(pobre = ifelse(pobre_lab=="Yes",1,0)) %>% select(id, pobre)
     
-    write.csv(predict, "Nombredemodelo.csv", row.names = FALSE)
+    write.csv(predictSample, "Logit.csv", row.names = FALSE)
   
