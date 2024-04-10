8af1eaa0a53eb1d06e421bc0a820a74a59d4272a
Dago000 <de.gonzalezg1@uniandes.edu.co>
2024-04-10 12:16:15 -0500

    Update PS2 Clasificacion.R

--- PS2 Clasificacion.R
+++ PS2 Clasificacion.R
@@ -21,73 +21,97 @@ test_personas<-read.csv("data/test_personas.csv")
 colnames(train_hogares)
 colnames(train_personas)
 
-#Preprocesamiento Personas AÑADIR MUCHAS MÁS
+#Preprocesamiento Personas
 
 ptrain_personas<- train_personas %>% mutate(
-  mujer = ifelse(P6020==2,1,0), # 1 si es Mujer
-  H_Head = ifelse(P6050== 1, 1, 0), # 1 si es jefe de hogar
+  genero = ifelse(P6020==2,1,0),# 1 si es hombre
+  cabecera = Clase,# 1 si reside en áreas urbanas
+  edad=P6040,
+  jefe = ifelse(P6050== 1, 1, 0), # 1 si es jefe de hogar
   menor = ifelse(P6040<=6,1,0), # Menores de 6 años
-  EducLevel = ifelse(P6210==9,0,P6210), #No sabe no responde es 0
+  EducLevel = ifelse(P6210==9|P6210==0,0,P6210), #No sabe no responde es 0
   ocupado = ifelse(is.na(Oc),0,1), 
   contributivo=ifelse(P6090==1|P6090==2,1,0), #No sabe no responde es 0 (1 )
   trabaja=ifelse(P6240==1,1,0), # 1 si el individuo trabaja
   Oficio=ifelse(is.na(Oficio),0,Oficio), 
   trabaja_solo=ifelse(P6870==1,1,0), # 1 Si el individuo trabaja solo
-  h_trabajadas=ifelse(is.na(P6800),0,P6800)
+  h_trabajadas=ifelse(is.na(P6800),0,P6800),
+  tiempotrab = P6426,# Tiempo que lleva trabajando en la empresa
+  sin_pension = ifelse((P7500s2 == 2 | is.na(P7500s2)) & ((edad >= 62 & genero == 0) | (edad >= 57 & genero == 1)), 1, 0), #Sin pension cuando debería
+  p=P7500s2,
+  pet=ifelse(Pet==1,1,0)# Hace parte de la pob en edad de trabajar
   ) %>% 
-  select(id, Orden,mujer,H_Head,menor,EducLevel,trabaja_solo,h_trabajadas,ocupado,contributivo,trabaja,Oficio)
+select(id, Orden,genero,edad,cabecera,jefe,menor,EducLevel,trabaja_solo,contributivo,h_trabajadas,ocupado,contributivo,trabaja,Oficio,tiempotrab,sin_pension,pet)
+
 colnames(ptrain_personas)
 
 #Replica para la base de test
 
 ptest_personas<- test_personas %>% mutate(
-  mujer = ifelse(P6020==2,1,0), # 1 si es Mujer
-  H_Head = ifelse(P6050== 1, 1, 0), # 1 si es jefe de hogar
+  genero = ifelse(P6020==2,1,0),# 1 si es hombre
+  cabecera = Clase,# 1 si reside en áreas urbanas
+  edad=P6040,
+  jefe = ifelse(P6050== 1, 1, 0), # 1 si es jefe de hogar
   menor = ifelse(P6040<=6,1,0), # Menores de 6 años
-  EducLevel = ifelse(P6210==9,0,P6210), #No sabe no responde es 0
+  EducLevel = ifelse(P6210==9|P6210==0,0,P6210), #No sabe no responde es 0
   ocupado = ifelse(is.na(Oc),0,1), 
   contributivo=ifelse(P6090==1|P6090==2,1,0), #No sabe no responde es 0 (1 )
   trabaja=ifelse(P6240==1,1,0), # 1 si el individuo trabaja
   Oficio=ifelse(is.na(Oficio),0,Oficio), 
   trabaja_solo=ifelse(P6870==1,1,0), # 1 Si el individuo trabaja solo
-  h_trabajadas=ifelse(is.na(P6800),0,P6800)
+  h_trabajadas=ifelse(is.na(P6800),0,P6800),
+  tiempotrab = P6426,# Tiempo que lleva trabajando en la empresa
+  sin_pension = ifelse((P7500s2 == 2 | is.na(P7500s2)) & ((edad >= 62 & genero == 0) | (edad >= 57 & genero == 1)), 1, 0), #Sin pension cuando debería
+  p=P7500s2,
+  pet=ifelse(Pet==1,1,0)# Hace parte de la pob en edad de trabajar
 ) %>% 
-  select(id, Orden,mujer,H_Head,menor,EducLevel,trabaja_solo,h_trabajadas,ocupado,contributivo,trabaja,Oficio)
+  select(id, Orden,genero,edad,cabecera,jefe,menor,EducLevel,trabaja_solo,contributivo,h_trabajadas,ocupado,contributivo,trabaja,Oficio,tiempotrab,sin_pension,pet)
+
 colnames(ptest_personas)
 
 #Preprocesamiento Hogares
 train_personas_nivel_hogar<- ptrain_personas %>% 
   group_by(id) %>% 
-  summarize(nmujeres=sum(mujer,na.rm=TRUE),
+  summarize(nmujeres=sum(genero,na.rm=TRUE),
             nmenores=sum(menor,na.rm=TRUE),
             maxEducLevel=max(EducLevel,na.rm=TRUE),
-            nocupados=sum(ocupado,na.rm=TRUE)
+            nocupados=sum(ocupado,na.rm=TRUE),
+            rel_pet=sum(pet,na.rm=TRUE)/max(Orden,na.rm=TRUE),
+            trab_por_persona=sum(h_trabajadas)/max(Orden,na.rm=TRUE)
   )
 
 train_personas_hogar<- ptrain_personas %>% 
-  filter(H_Head==1) %>% 
-  select(id,mujer,EducLevel,ocupado) %>% 
-  rename(H_Head_mujer=mujer,
-         H_Head_Educ_level=EducLevel,
-         H_Head_ocupado=ocupado) %>% 
+  filter(jefe==1) %>% 
+  select(id,genero,EducLevel,ocupado,trabaja_solo,contributivo,edad,genero,sin_pension) %>% 
+  rename(jefe_mujer=genero,
+         jefe_Educ_level=EducLevel,
+         jefe_ocupado=ocupado,
+         jefe_solo=trabaja_solo,
+         jefe_contrib=contributivo,
+         jefe_sin_pension=sin_pension) %>% 
   left_join(train_personas_nivel_hogar)
 
 
 #Replica para la base de test
 test_personas_nivel_hogar<- ptest_personas %>% 
   group_by(id) %>% 
-  summarize(nmujeres=sum(mujer,na.rm=TRUE),
+  summarize(nmujeres=sum(genero,na.rm=TRUE),
             nmenores=sum(menor,na.rm=TRUE),
             maxEducLevel=max(EducLevel,na.rm=TRUE),
-            nocupados=sum(ocupado,na.rm=TRUE)
+            nocupados=sum(ocupado,na.rm=TRUE),
+            rel_pet=sum(pet,na.rm=TRUE)/max(Orden,na.rm=TRUE),
+            trab_por_persona=sum(h_trabajadas)/max(Orden,na.rm=TRUE)
   )
 
 test_personas_hogar<- ptest_personas %>% 
-  filter(H_Head==1) %>% 
-  select(id,mujer,EducLevel,ocupado) %>% 
-  rename(H_Head_mujer=mujer,
-         H_Head_Educ_level=EducLevel,
-         H_Head_ocupado=ocupado) %>% 
+  filter(jefe==1) %>% 
+  select(id,genero,EducLevel,ocupado,trabaja_solo,contributivo,edad,genero,sin_pension) %>% 
+  rename(jefe_mujer=genero,
+         jefe_Educ_level=EducLevel,
+         jefe_ocupado=ocupado,
+         jefe_solo=trabaja_solo,
+         jefe_contrib=contributivo,
+         jefe_sin_pension=sin_pension) %>% 
   left_join(test_personas_nivel_hogar)
 
 #Criterio de Pobreza del DANE
@@ -115,16 +139,29 @@ test<- ptest_hogares %>%
 train<- train %>% 
   mutate(Pobre=factor(Pobre,levels=c(0,1),labels=c("No","Yes")),
          Dominio=factor(Dominio),
-         H_Head_Educ_level=factor(H_Head_Educ_level,levels=c(0:6), labels=c("Ns",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria')),
-         maxEducLevel=factor(maxEducLevel,levels=c(0:6), labels=c("Ns",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria'))
+         jefe_Educ_level=factor(jefe_Educ_level,levels=c(0:6), labels=c("Ns",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria')),
+         maxEducLevel=factor(maxEducLevel,levels=c(0:6), labels=c("Ns",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria')),
+         jefe_contrib=factor(jefe_contrib),
+         jefe_sin_pension=factor(jefe_sin_pension),
+         jefe_mujer=factor(jefe_mujer),
+         jefe_ocupado=factor(jefe_ocupado),
+         jefe_solo=factor(jefe_solo),
+         
   )
 #Para Test
 test<- test %>% 
   mutate(Dominio=factor(Dominio),
-         H_Head_Educ_level=factor(H_Head_Educ_level,levels=c(0:6), labels=c("Ns",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria')),
-         maxEducLevel=factor(maxEducLevel,levels=c(0:6), labels=c("Ns",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria'))
+         jefe_Educ_level=factor(jefe_Educ_level,levels=c(0:6), labels=c("Ns",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria')),
+         maxEducLevel=factor(maxEducLevel,levels=c(0:6), labels=c("Ns",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria')),
+         jefe_contrib=factor(jefe_contrib),
+         jefe_sin_pension=factor(jefe_sin_pension),
+         jefe_mujer=factor(jefe_mujer),
+         jefe_ocupado=factor(jefe_ocupado),
+         jefe_solo=factor(jefe_solo),
+         
   )
-colnames(train)
+
+
 #Entrenamiento del modelo
 
 set.seed(2618)
@@ -136,7 +173,6 @@ arbol_clasificacion_rpart <- rpart(Pobre~.,
 arbol_clasificacion_rpart
 
 #Arbol gráfico
-
 prp(arbol_clasificacion_rpart, under = TRUE, branch.lty = 2, yesno = 2, faclen = 0, varlen=15,box.palette = "-RdYlGn")
 
 
@@ -153,3 +189,4 @@ predictSample<- predictSample %>%
   select(id,pobre)
 
 write.csv(predictSample,"classification_CARTS.csv", row.names = FALSE)
+ 
