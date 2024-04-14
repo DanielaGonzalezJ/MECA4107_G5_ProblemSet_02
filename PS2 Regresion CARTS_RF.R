8818b3196881f56c08850ded9b064ddaf6db6f22
Dago000 <de.gonzalezg1@uniandes.edu.co>
2024-04-14 12:19:19 -0500

    Ultimas entregas Kaggle

--- PS2 Regresion CARTS_RF.R
+++ PS2 Regresion CARTS_RF.R
@@ -1,6 +1,5 @@
-###  Modelos de Clasificación Cart//Random Forest
+###  Modelos de Regresión Cart//Random Forest
 #Según la base del DANE un hogar es clasificado pobre si el “Ingreso percápita de la unidad de gasto con imputación de arriendo a propietarios y usufructuarios” es menor a la Linea de pobreza que le corresponde al hogar.
-
 #rm(list = ls())
 
 if(!require(pacman)) install.packages("pacman") ; require(pacman)
@@ -40,8 +39,8 @@ ptrain_personas<- train_personas %>% mutate(
   tiempotrab = P6426,# Tiempo que lleva trabajando en la empresa
   sin_pension = ifelse((P7500s2 == 2 | is.na(P7500s2)) & ((edad >= 62 & genero == 0) | (edad >= 57 & genero == 1)), 1, 0), #Sin pension cuando debería
   pet=ifelse(Pet==1,1,0)# Hace parte de la pob en edad de trabajar
-  ) %>% 
-select(id, Orden,genero,edad,cabecera,jefe,menor,EducLevel,trabaja_solo,contributivo,h_trabajadas,ocupado,contributivo,trabaja,Oficio,tiempotrab,sin_pension,pet)
+) %>% 
+  select(id, Orden,genero,edad,cabecera,jefe,menor,EducLevel,trabaja_solo,contributivo,h_trabajadas,ocupado,contributivo,trabaja,Oficio,tiempotrab,sin_pension,pet)
 
 colnames(ptrain_personas)
 
@@ -68,7 +67,7 @@ ptest_personas<- test_personas %>% mutate(
 
 colnames(ptest_personas)
 
-#Preprocesamiento Hogares
+#Preprocesamiento de personas a hogares
 train_personas_nivel_hogar<- ptrain_personas %>% 
   group_by(id) %>% 
   summarize(nmujeres=sum(genero,na.rm=TRUE),
@@ -89,7 +88,7 @@ train_personas_hogar<- ptrain_personas %>%
          jefe_contrib=contributivo,
          jefe_sin_pension=sin_pension,
          jefe_edad=edad,
-         ) %>% 
+  ) %>% 
   left_join(train_personas_nivel_hogar)
 
 
@@ -114,18 +113,20 @@ test_personas_hogar<- ptest_personas %>%
          jefe_contrib=contributivo,
          jefe_sin_pension=sin_pension,
          jefe_edad=edad,
-         ) %>% 
+  ) %>% 
   left_join(test_personas_nivel_hogar)
 
-#Criterio de Pobreza del DANE
+#Preprocesamiento de Hogares
 ptrain_hogares<- train_hogares %>% 
-  mutate(arrienda=ifelse(P5090==3,1,0)) %>% 
-  select(id,Dominio,arrienda,Pobre)
+  mutate(arrienda=ifelse(P5090==3,1,0),
+         cost_arrienda=ifelse(is.na(P5130),P5140,P5130)) %>% 
+  select(id,Dominio,arrienda,Ingpcug,cost_arrienda)
 
 
 ptest_hogares<- test_hogares %>% 
-  mutate(arrienda=ifelse(P5090==3,1,0)) %>% 
-  select(id,Dominio,arrienda) 
+  mutate(arrienda=ifelse(P5090==3,1,0),
+         cost_arrienda=ifelse(is.na(P5130),P5140,P5130)) %>% 
+  select(id,Dominio,arrienda,cost_arrienda) 
 
 #Unificamos la base de hogares a partir de la información de los hogares
 
@@ -136,12 +137,10 @@ train<- ptrain_hogares %>%
 test<- ptest_hogares %>% 
   left_join(test_personas_hogar)
 
-
 #Convertimos las variables categóricas en factores
 #Para Train
 train<- train %>% 
-  mutate(Pobre=factor(Pobre,levels=c(0,1),labels=c("No","Yes")),
-         arrienda=factor(arrienda),
+  mutate(arrienda=factor(arrienda),
          Dominio=factor(Dominio),
          jefe_Educ_level=factor(jefe_Educ_level,levels=c(0:6), labels=c("NS",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria')),
          maxEducLevel=factor(maxEducLevel,levels=c(0:6), labels=c("Ns",'Ninguno', 'Preescolar','Primaria', 'Secundaria','Media', 'Universitaria')),
@@ -166,119 +165,80 @@ test<- test %>%
          
   )
 
-#Modelos de Clasifiación
-#Entrenamiento del modelo Carts con CV para penalización
+
+#Modelos de Regresión
+#CART
 
 fiveStats <- function(...) c(twoClassSummary(...), defaultSummary(...))  ## Para usar ROC) (u otras más) para tuning
 set.seed(2618)
 ctrl<- trainControl(method = "cv",
-                    number = 5,
-                    summaryFunction = fiveStats,
-                    classProbs = TRUE, 
-                    verbose=FALSE,
-                    savePredictions = T)
+                    number = 5)
 
-# Especificamos la grilla de los alphas
-grid <- expand.grid(cp = seq(0, 0.03, 0.001))
+# Especificamos la grilla 
+grid <- expand.grid(maxdepth = seq(1,20,1))
 
-cv_tree <- train(Pobre~.,
+cv_tree <- train(Ingpcug~.,
                  data = train,
-                 method = "rpart", 
+                 method = "rpart2", 
                  trControl = ctrl, 
                  tuneGrid = grid, 
-                 metric= "ROC"
+                 metric= "RMSE"
 )
 cv_tree
 
-cv_tree$bestTune$cp
-
-#Arbol gráfico
-#prp(cv_tree$finalModel, under = TRUE, branch.lty = 2, yesno = 2, faclen = 0, varlen=15,box.palette = "-RdYlGn")
 
 #Envío para Kagglee
 
 predictSample <- test   %>% 
-  mutate(pobre_lab = predict(cv_tree, newdata = test, type = "raw")    ## predicted class labels
-  )  %>% select(id,pobre_lab)
-
-head(predictSample)
+  mutate(Ingpcug = predict(cv_tree, newdata = test)    ## predicted class labels
+  )  %>% select(id,Ingpcug)
 
 predictSample<- predictSample %>% 
-  mutate(pobre=ifelse(pobre_lab=="Yes",1,0)) %>% 
+  left_join(test_hogares) %>% 
+  select(id,Ingpcug,Lp)
+
+predictSample<- predictSample %>% 
+  mutate(pobre=ifelse(Ingpcug<Lp,1,0)) %>% 
   select(id,pobre)
+summary(predictSample)
 
-
-write.csv(predictSample,"classification_CARTS_5.csv", row.names = FALSE)
-
+write.csv(predictSample,"regression_CARTS_1.csv", row.names = FALSE)
 
 # Entrenamiento del Modelo Random Forest
 # Definimos el proceso de CV
 
 set.seed(2618)
-fiveStats <- function(...) c(twoClassSummary(...), defaultSummary(...))
 ctrl<- trainControl(method = "cv",
-                    number = 5,
-                    summaryFunction = fiveStats,
-                    classProbs = TRUE,
-                    verbose=FALSE,
-                    savePredictions = T)
+                    number = 5)
 
-mtry_grid<-expand.grid(mtry =c(8,11,15), # 
-                       min.node.size= c(50,60,70,80), #controla la complejidad del arbol
-                       splitrule= 'gini') #splitrule fija en gini. 
+mtry_grid<-expand.grid(mtry =c(15,18,20), # c(8,11,15)
+                       min.node.size= c(15,20,25,30,35), #controla la complejidad del arbol
+                       splitrule= 'variance') #splitrule fija en gini. 
 mtry_grid
 
-cv_RForest <- train(Pobre~., 
+cv_RForest <- train(Ingpcug~., 
                     data = train, 
                     method = "ranger",
                     trControl = ctrl,
-                    metric="ROC",
+                    metric="RMSE",
                     tuneGrid = mtry_grid,
-                    ntree=500)
+                    ntree=500,
+                    importance="impurity")
 cv_RForest
 
-#Ver el modelo fianl
-cv_RForest$finalModel
+#Envío para Kagglee
 
-#Vemos cuales son los predictores mas importantes:
-
-
-#Envío Kaggle
 predictSample <- test   %>% 
-  mutate(pobre_lab = predict(cv_RForest, 
-                   newdata = test, 
-                   type="raw"))  %>% select(id,pobre_lab)
-
-head(predictSample)
+  mutate(Ingpcug = predict(cv_RForest, newdata = test)    ## predicted class labels
+  )  %>% select(id,Ingpcug)
 
 predictSample<- predictSample %>% 
-  mutate(pobre=ifelse(pobre_lab=="Yes",1,0)) %>% 
+  left_join(test_hogares) %>% 
+  select(id,Ingpcug,Lp)
+
+predictSample<- predictSample %>% 
+  mutate(pobre=ifelse(Ingpcug<Lp,1,0)) %>% 
   select(id,pobre)
+summary(predictSample)
 
-write.csv(predictSample,"classification_RandomForest_4.csv", row.names = FALSE)
-
-#Modelos de Regresión
-
-fiveStats <- function(...) c(twoClassSummary(...), defaultSummary(...))  ## Para usar ROC) (u otras más) para tuning
-set.seed(2618)
-ctrl<- trainControl(method = "cv",
-                    number = 5,
-                    summaryFunction = fiveStats,
-                    classProbs = TRUE, 
-                    verbose=FALSE,
-                    savePredictions = T)
-
-# Especificamos la grilla de los alphas
-grid <- expand.grid(cp = seq(0, 0.03, 0.001))
-
-cv_tree <- train(Pobre~.,
-                 data = train,
-                 method = "rpart", 
-                 trControl = ctrl, 
-                 tuneGrid = grid, 
-                 metric= "ROC"
-)
-cv_tree
-
-cv_tree$bestTune$cp
-
+write.csv(predictSample,"regression_RandomForest_1.csv", row.names = FALSE)
